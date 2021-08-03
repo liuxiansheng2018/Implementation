853166c45ea850daf3c5e4fe29772d581078106f
刘豪 <lh@dataqin.com>
2021-08-03 09:30:09 +0800

    feat: 自动化部署测试

---
+++ release/index.js
@@ -0,0 +1,40 @@
+const chalk = require('chalk')
+const execa = require('execa')
+const {
+    prompt
+} = require('enquirer')
+
+
+const setup = msg => console.log(chalk.cyan(msg))
+
+const run = (bin, args, opts={}) => 
+execa(bin, args, {
+    stdio: 'inherit',
+    ...opts
+})
+
+async function main () {
+    console.log('欢迎使用发版小助手脚本')
+    setup('\n 验证是否提交代码...')
+    const { yes } = await prompt({
+        type: 'confirm',
+        name: 'yes',
+        message: `确定已经提交代码并将代码push到develop分支了吗?`
+      })
+    
+    if( !yes ) {
+        return 
+    }
+    await run('git', ['checkout', 'develop'])
+    await run ('git', ['pull', 'origin', 'develop'])
+
+    await run('git', ['checkout', 'release'])
+    await run('git', ['pull', 'origin', 'release'])
+
+    await run('git', ['merge', 'develop'])
+    await run('git', ['push', 'origin', 'release'])
+}
+
+main().catch(err => {
+    console.log(err)
+})
